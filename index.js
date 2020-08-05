18eac2a9d146c941de23e6a3933c8189c0a024d2
Jason Wright <jwright@speareducation.com>
2020-08-05 15:45:38 -0700

    GL-1483 fixed createInvalidation Quantity issue

--- index.js
+++ index.js
@@ -6,11 +6,6 @@ const cloudfront = new AWS.CloudFront();
 const handle = async () => {
     const distributions = JSON.parse(core.getInput('distributions'));
     const originId = core.getInput('originId');
-    console.log({
-        context: github.context,
-        repo: github.context.repo,
-        env: process.env,
-    });
     const project = github.context.repo.repo || null;
     const branch = github.context.ref.replace('refs/heads/', '')
     const environment = branch.split('/')[1] || null;
@@ -21,12 +16,14 @@ const handle = async () => {
     }
 
     // get old configuration
+    console.log('Fetching old Distribution')
     const result = await cloudfront.getDistributionConfig({ Id: distributions[environment] }).promise();
     const { ETag, DistributionConfig } = result
     
     // upload configuration with new OriginPath
     // For help with this, see ./example-cloudfront-config.json
     // or use aws-spear cloudfront get-distribution-config --id= "<DIST ID>" | tee
+    console.log('Updating Distribution');
     await cloudfront.updateDistribution({
         Id: distributions[environment],
         IfMatch: ETag,
@@ -43,16 +40,19 @@ const handle = async () => {
     });
 
     // invalidate the cache
+    console.log('Invalidating Cache')
     await cloudfront.createInvalidation({
         DistributionId: distributions[environment],
         InvalidationBatch: {
             Paths: {
-                Quantity: 10000, // arbitrary number
+                Quantity: 1, // must match number of entries in "Items"
                 Items: ['/*'],
             },
             CallerReference: branch // use release branch as invalidation reference
         }
     }).promise();
+
+    console.log('Done!');
 };
 
 
